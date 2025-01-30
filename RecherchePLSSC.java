8d7d49ba296709ca0310a028a18eb56640389fc8
barkoko <barkokomar@gmail.com>
2025-01-30 19:29:58 +0100

    fini algo naive (testee a la main)

--- RecherchePLSSC.java
+++ RecherchePLSSC.java
@@ -8,14 +8,31 @@ public class RecherchePLSSC {
 
     // Recherche d'une PLSSC de 2 chaînes, naïf
     static String PLSSC(String S1, String S2) {
-        // à modifier
-        return S2;
+
+		if (S1.isEmpty() || S2.isEmpty()) {
+			return "";
+		}
+
+		String new_S1 = (S1.length() > 1) ? S1.substring(1) : "";
+		String new_S2 = (S2.length() > 1) ? S2.substring(1) : "";
+
+		if ( S1.charAt(0) == S2.charAt(0) ) {
+			StringBuilder subseq = new StringBuilder();
+			subseq.append(S1.charAt(0));
+			subseq.append(PLSSC(new_S1, new_S2));
+			return subseq.toString();
+		}
+
+		String subseq1 = PLSSC(new_S1, S2);
+		String subseq2 = PLSSC(S1, new_S2);
+
+		return subseq1.length() > subseq2.length() ? subseq1 : subseq2;
     }
 
     // Recherche d'une PLSSC de 2 chaînes, prog. dyn.
     static String PLSSC_PD(String S1, String S2) {
         // à modifier
-        return S1;
+        return "";
     }
 
 
@@ -46,6 +63,8 @@ public class RecherchePLSSC {
                 // date de fin pour le calcul du temps écoulé
                 long endTime = System.nanoTime();
 
+                System.out.println("S1: " + S1);
+                System.out.println("S2: " + S2);
                 System.out.println("PLSSC: " + result);
 
                 // Impression de la longueur du S1 de S2 et du temps d'exécution
